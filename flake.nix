feedf22625005c62b33c3b9ec1d770e5e4803a08
LeixB <abone9999@gmail.com>
2022-05-04 20:03:13 +0200

    Generate README with nix

---
+++ flake.nix
@@ -0,0 +1,84 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+      lib = pkgs.lib;
+    in rec {
+      devShell = with pkgs; mkShellNoCC {
+        name = "grip";
+        buildInputs = [ python3.pkgs.grip ];
+      };
+
+      packages.README = let
+        # tools = lib.sort (a: b: lib.toLower a.name < lib.toLower b.name) [
+        tools = lib.sort (a: b: a.familiarity > b.familiarity) (import ./tools.nix);
+
+        makeBadge = { url, name, img, size ? 25 }: ''
+          <a href="${url}" title="${name}" style="display:flex;">
+            <img src="${img}" alt="${name}" width=${toString size} height=${toString size}>
+          </a>
+        '';
+
+        makeBadgeRow = tools:
+          ''<p align="left">'' +
+            lib.concatMapStrings (tool: makeBadge {inherit (tool) url name img;} ) tools
+          + "</p>\n";
+
+        tools_section = with builtins; "### :rocket: Things I code with\n\n"
+          + makeBadgeRow tools
+        ;
+
+        username = "LeixB";
+        stats_image = "https://github-readme-stats.vercel.app/api?username=${username}&show_icons=true&count_private=true";
+        stats = ''
+          [<img align="right" width="50%" src="${stats_image}&theme=dark">](https://metrics.lecoq.io/${username}#gh-dark-mode-only)
+          [<img align="right" width="50%" src="${stats_image}">](https://metrics.lecoq.io/${username}#gh-light-mode-only)
+        '';
+
+        badges = ''
+          [![built with nix](https://img.shields.io/static/v1?logo=nixos&logoColor=white&label=&message=Built%20with%20Nix&color=41439a)](https://builtwithnix.org "built with nix")
+          ![visitors](https://visitor-badge.glitch.me/badge?page_id=${username}.${username})
+        '';
+
+        about = ''
+          I'm LeixB, a Linux enthusiast and NixOS contributor.
+
+          - :snowflake: I'm a fan of [Nix](https://nixos.org) and [NixOS](https://nixos.org/nixos/).
+          - 🔭 I’m currently exploring ways to make data science projects more reproducible.
+          - 🌱 I’m currently learning Rust, Go and Nix.
+          - :mortar_board: I'm enrolled in the master in data science at [UPC](https://www.upc.edu).
+          - 😄 Pronouns: he/him
+        '';
+
+        text = ''
+        <h1 aligh="center">Hi there, nice to see you! :wave:</h1>
+        </br>
+
+        ${badges}
+
+        ${stats}
+
+        ${about}
+
+        ${tools_section}
+        '';
+      in pkgs.writeTextFile {
+        name = "github-profile";
+        destination = "/README.md";
+        inherit text;
+      };
+
+      defaultPackage = packages.README;
+    });
+}
---
+++ tools.nix
@@ -0,0 +1,215 @@
+let
+  _devicon = name: suffix: "https://raw.githubusercontent.com/devicons/devicon/master/icons/${name}/${name}-${suffix}.svg";
+  devicon = name: _devicon name "original";
+in [
+  {
+    name = "Angular";
+    url = "https://angular.io/";
+    img = devicon "angularjs";
+    categories = ["Frontend" "Framework"];
+    familiarity = 4;
+  }
+  {
+    name = "Nix";
+    url = "https://nixos.org/nix/";
+    img = devicon "nixos";
+    categories = ["DevOps" "Language"];
+    familiarity = 7;
+  }
+  {
+    name = "bash";
+    url = "https://www.gnu.org/software/bash/";
+    img = devicon "bash";
+    categories = ["Language"];
+    familiarity = 7;
+  }
+  {
+    name = "C";
+    url = "https://www.gnu.org/software/c/";
+    img = devicon "c";
+    categories = ["Language"];
+    familiarity = 6;
+  }
+  {
+    name = "C++";
+    url = "https://www.gnu.org/software/gcc/";
+    img = devicon "cplusplus";
+    categories = ["Language"];
+    familiarity = 6;
+  }
+  {
+    name = "CMake";
+    url = "https://cmake.org/";
+    img = devicon "cmake";
+    categories = [ "Build" ];
+    familiarity = 3;
+  }
+  {
+    name = "Docker";
+    url = "https://docker.com/";
+    img = devicon "docker";
+    categories = ["DevOps"];
+    familiarity = 5;
+  }
+  {
+    name = "Podman";
+    url = "https://podman.io/";
+    img = devicon "podman";
+    categories = ["DevOps"];
+    familiarity = 5;
+  }
+  {
+    name = "Python";
+    url = "https://www.python.org/";
+    img = devicon "python";
+    categories = ["Language"];
+    familiarity = 7;
+  }
+  {
+    name = "Rust";
+    url = "https://www.rust-lang.org/";
+    img = _devicon "rust" "plain";
+    categories = ["Language"];
+    familiarity = 8;
+  }
+  {
+    name = "Go";
+    url = "https://golang.org/";
+    img = devicon "go";
+    categories = ["Language"];
+    familiarity = 8;
+  }
+  {
+    name = "Java";
+    url = "https://www.oracle.com/technetwork/java/index.html";
+    img = devicon "java";
+    categories = ["Language"];
+    familiarity = 6;
+  }
+  {
+    name = "R";
+    url = "https://www.r-project.org/";
+    img = devicon "r";
+    categories = ["Language" "Data Science"];
+    familiarity = 6;
+  }
+  {
+    name = "Julia";
+    url = "https://julialang.org/";
+    img = devicon "julia";
+    categories = ["Language" "Data Science"];
+    familiarity = 6;
+  }
+  {
+    name = "Haskell";
+    url = "https://www.haskell.org/";
+    img = devicon "haskell";
+    categories = ["Language"];
+    familiarity = 5;
+  }
+  {
+    name = "Vim";
+    url = "https://vim.org/";
+    img = devicon "vim";
+    categories = ["Editor"];
+    familiarity = 8;
+  }
+  {
+    name = "Neovim";
+    url = "https://neovim.io/";
+    img = "https://github.com/neovim/neovim.github.io/blob/master/logos/neovim-mark-flat.svg";
+    categories = ["Editor"];
+    familiarity = 8;
+  }
+  {
+    name = "LaTeX";
+    url = "https://www.latex-project.org/";
+    img = devicon "latex";
+    categories = ["Language"];
+    familiarity = 8;
+  }
+  {
+    name = "GitHub Actions";
+    url = "https://github.com/features/actions";
+    img = devicon "github";
+    categories = ["DevOps"];
+    familiarity = 6;
+  }
+  {
+    name = "git";
+    url = "https://git-scm.com/";
+    img = devicon "git";
+    categories = ["DevOps"];
+    familiarity = 7;
+  }
+  {
+    name = "AWS";
+    url = "https://aws.amazon.com/";
+    img = devicon "amazonwebservices";
+    categories = ["DevOps" "Cloud"];
+    familiarity = 6;
+  }
+  {
+    name = "Google Cloud";
+    url = "https://cloud.google.com/";
+    img = devicon "googlecloud";
+    categories = ["DevOps" "Cloud"];
+    familiarity = 4;
+  }
+  {
+    name = "MongoDB";
+    url = "https://www.mongodb.com/";
+    img = devicon "mongodb";
+    categories = ["Database"];
+    familiarity = 6;
+  }
+  {
+    name = "PostgreSQL";
+    url = "https://www.postgresql.org/";
+    img = devicon "postgresql";
+    categories = ["Database"];
+    familiarity = 6;
+  }
+  {
+    name = "ELM";
+    url = "https://elm-lang.org/";
+    img = devicon "elm";
+    categories = ["Language"];
+    familiarity = 6;
+  }
+  {
+    name = "Linux";
+    url = "https://www.linux.org/";
+    img = devicon "linux";
+    categories = ["DevOps" "OS"];
+    familiarity = 8;
+  }
+  {
+    name = "matlab";
+    url = "https://www.mathworks.com/products/matlab.html";
+    img = devicon "matlab";
+    categories = ["Data Science"];
+    familiarity = 6;
+  }
+  {
+    name = "neo4j";
+    url = "https://neo4j.com/";
+    img = devicon "neo4j";
+    categories = ["Database"];
+    familiarity = 6;
+  }
+  {
+    name = "numPy";
+    url = "https://numpy.org/";
+    img = devicon "numpy";
+    categories = ["Data Science"];
+    familiarity = 6;
+  }
+  {
+    name = "pandas";
+    url = "https://pandas.pydata.org/";
+    img = devicon "pandas";
+    categories = ["Data Science"];
+    familiarity = 6;
+  }
+]
